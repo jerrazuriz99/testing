1edb1dcaabd2f55b057137b7c9db4775b299940d
JuanSa <juan.vg@uc.cl>
2023-03-28 18:17:37 -0300

    Agregando minusequal

--- Tarea2/rewriter/minus_equals_rewriter.py
+++ Tarea2/rewriter/minus_equals_rewriter.py
@@ -6,9 +6,11 @@ class MinusEqualsTransformer(NodeTransformer):
         NodeTransformer.generic_visit(self, node)
         if isinstance(node.value, BinOp):
             if isinstance(node.value.op, Sub):
-                if isinstance(node.value.left, Name) and isinstance(node.value.right, Name):
+                if isinstance(node.value.left, Name):
                     if node.value.left.id == node.targets[0].id:
-                        node.value = AugAssign(target=node.targets[0], op=Sub(), value=node.value.right)
+                        """ print(f"Reemplazando {node.value.left.id} por {node.targets[0].id}") """
+                        node = AugAssign(target=node.targets[0], op=Sub(), value=node.value.right)
+
         return node
 
 class MinusEqualsRewriterCommand(RewriterCommand):
