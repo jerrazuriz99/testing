5853cb0e4deb2cd7af33d7c2bbfa6bfb9357f9c8
Joaquín Errázuriz McKay <joaquinerrazurizmckay@MacBook-Pro-de-Joaquin.local>
2023-04-08 11:14:09 -0400

    parte 1 casi lista

--- Tarea3/instrumentor/classInstrumentor.py
+++ Tarea3/instrumentor/classInstrumentor.py
@@ -5,6 +5,9 @@ from profiler import Profiler
 
 class ClassInstrumentor(NodeTransformer):
 
+    def __init__(self):
+        self.current_class = None
+
     def visit_Module(self, node: Module):
         transformedNode = NodeTransformer.generic_visit(self, node)
         import_profile_injected = parse(
@@ -12,7 +15,10 @@ class ClassInstrumentor(NodeTransformer):
         transformedNode.body.insert(0, import_profile_injected.body[0])
         fix_missing_locations(transformedNode)
 
+        return transformedNode
+
     def visit_ClassDef(self, node: ClassDef):
+        self.current_class = node.name
         transformedNode = NodeTransformer.generic_visit(self, node)
         # recorremos los metodos de la clase
         for method in transformedNode.body:
@@ -30,55 +36,45 @@ class ClassInstrumentor(NodeTransformer):
 
         return transformedNode
 
+    # Recorrer las funciones y buscar rectangle1 = Rectangle(2, 3), assert rectangle1 != rectangle2, 'Different rectangles' o assert rectangle.get_area() == 6, 'Incorrect area'
     def visit_FunctionDef(self, node: FunctionDef):
         self.function_name = node.name
         transformedNode = NodeTransformer.generic_visit(self, node)
         return transformedNode
 
-    def visit_Call(self, node: Call):
+    def visit_Assign(self, node: Assign):
         transformedNode = NodeTransformer.generic_visit(self, node)
-        print("=======================================")
-        print("transformedNode: " + unparse(transformedNode))
-        if isinstance(transformedNode.func, Attribute) and isinstance(transformedNode.func.value, Name):
-            # Encontrar la función que se llama
-            function_name = transformedNode.func.attr
-            print("function_name: " + function_name)
-            # Encontrar el nombre de la clase que se llama
-            class_name = transformedNode.func.value.id
-            print("class_name: " + class_name)
-            # Encontrar el número de línea de la llamada
-            line_number = transformedNode.lineno
-            print("line_number: " + str(line_number))
-            # Encontrar el nombre del método que llama a la función
-            method_name = self.function_name
-            print("method_name: " + method_name)
+        # Si es una asignacion de una clase, inyectamos codigo para llamar al profiler
+        if isinstance(transformedNode.value, Call):
+            if isinstance(transformedNode.value.func, Name):
+                if transformedNode.value.func.id in self.current_class:
+                    method = '__init__'
+                    injectedCode = parse('ClassProfiler.recordUsed(\'' +
+                                         self.function_name + '\', ' + str(transformedNode.lineno) + ', \'' + self.current_class + ', ' + method + '\')')
+                    print(unparse(injectedCode))
+                    transformedNode = injectedCode.body[0]
+                    print(unparse(transformedNode))
+                    fix_missing_locations(transformedNode)
+        return transformedNode
 
+    def visit_Compare(self, node: Compare):
+        transformedNode = NodeTransformer.generic_visit(self, node)
+        # Si es una comparacion de una clase, inyectamos codigo para llamar al profiler
+        if isinstance(transformedNode.left, Name):
+            method = '__eq__'
             injectedCode = parse('ClassProfiler.recordUsed(\'' +
-                                 method_name + '\', ' + str(line_number) + ', \'' + class_name + '\', \'' + function_name + '\')')
-            print("injectedCode: " + unparse(injectedCode))
+                                 self.function_name + '\', ' + str(transformedNode.lineno) + ', \'' + self.current_class + ', ' + method + '\')')
+            print(unparse(injectedCode))
             transformedNode = injectedCode.body[0]
             fix_missing_locations(transformedNode)
-        # else if es un rectangle = Rectangle(2, 3), functionName = __init__
-        # else if es un rectangle1 != rectangle2, functionName = __eq__
-        elif isinstance(transformedNode.func, Name):
-            # Encontrar el nombre de la clase que se llama
-            class_name = transformedNode.func.id
-            print("class_name: " + class_name)
-            # Encontrar el número de línea de la llamada
-            line_number = transformedNode.lineno
-            print("line_number: " + str(line_number))
-            # Encontrar el nombre del método que llama a la función
-            method_name = self.function_name
-            print("method_name: " + method_name)
-            # Encontrar el nombre de la función que se llama
-            function_name = "__init__" if class_name == method_name else "__eq__"
-            print("function_name: " + function_name)
-
+        elif isinstance(transformedNode.left, Call):
+            method = transformedNode.left.func.attr
             injectedCode = parse('ClassProfiler.recordUsed(\'' +
-                                 method_name + '\', ' + str(line_number) + ', \'' + class_name + '\', \'' + function_name + '\')')
-            print("injectedCode: " + unparse(injectedCode))
+                                 self.function_name + '\', ' + str(transformedNode.lineno) + ', \'' + self.current_class + ', ' + method + '\')')
+            print(unparse(injectedCode))
             transformedNode = injectedCode.body[0]
             fix_missing_locations(transformedNode)
+        return transformedNode
 
 
 class ClassProfiler(Profiler):
@@ -87,8 +83,9 @@ class ClassProfiler(Profiler):
     def record(cls, methodName, lineNumber, className):
         cls.getInstance().ins_record(methodName, lineNumber, className)
 
+    @ classmethod
     def recordUsed(self, methodName, lineNumber, className, function):
-        self.getInstance().ins_record(methodName, lineNumber, className, function)
+        self.getInstance().ins_recordUsed(methodName, lineNumber, className, function)
 
     # Metodos de instancia
     def __init__(self):
@@ -100,6 +97,11 @@ class ClassProfiler(Profiler):
         if (methodName, lineNumber, className) not in self.methods_called:
             self.methods_called.append((methodName, lineNumber, className))
 
+    def ins_recordUsed(self, methodName, lineNumber, className, function):
+        if (methodName, lineNumber, className, function) not in self.methods_used:
+            self.methods_used.append(
+                (methodName, lineNumber, className, function))
+
     def report_executed_methods(self):
         print("-- Executed methods --")
         self.methods_called.sort(key=lambda t: t[1])
@@ -109,12 +111,13 @@ class ClassProfiler(Profiler):
         return self.methods_called
 
     def report_executed_by(self, function_name):
-        # Este metodo recibe el nombre de una funci´on ejecutada (no pertenece a una clase) y retorna una lista de tuplas que representan los m´etodos que fueron llamados por esa funci´on.
-        # La lista retornada esta ordenada descendentemente en base al segundo elemento de cada tupla (t[1]), donde cada tupla sigue el formato:
-        # (<method-name>,<codeline-methodDef>,
-        # <method-class>)
         print("-- Executed by --")
-        print("function_name: " + function_name)
+        self.methods_used.sort(key=lambda t: t[1])
+        for (fun, lineNumber, className, function) in self.methods_used:
+            if function == function_name:
+                print("Method " + fun + " in class " + className +
+                      " at line " + str(lineNumber))
+        return self.methods_used
 
 
 def instrument(ast):
