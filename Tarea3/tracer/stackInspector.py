f7b8e0467b99697e0289569e5b1ec33ac5583e07
Joaquín Errázuriz McKay <joaquinerrazurizmckay@MacBook-Pro-de-Joaquin.local>
2023-04-09 17:29:28 -0400

    cambios tracer

--- Tarea3/tracer/stackInspector.py
+++ Tarea3/tracer/stackInspector.py
@@ -1,8 +1,15 @@
 import traceback
+import sys
 """Clase que brinda funciones para facilitar la inspeccion de la pila"""
+
+
 class StackInspector:
 
+    def __init__(self, original, trace):
+        self.original_trace_function = original
+        self.trace_function = trace
     # Retorna verdad si el frame esta in la clase actual de inspeccion
+
     def our_frame(self, frame):
         return isinstance(frame.f_locals.get('self'), self.__class__)
 
@@ -14,10 +21,28 @@ class StackInspector:
     def is_internal_error(self, exc_tp, exc_value, exc_traceback):
         if not exc_tp:
             return False
-
         for frame, lineno in traceback.walk_tb(exc_traceback):
             if self.our_frame(frame):
                 return True
-
         return False
 
+    def traceit(self, frame, event: str, arg):
+        pass
+
+    # Esta funcion se llama al comienzo de un bloque 'with' y comienza con el rastreo
+    def __enter__(self):
+        self.original_trace_function = sys.gettrace()
+        sys.settrace(self.trace_function)
+        return self
+
+    # Esta funcion se llama al final de un bloque 'with' y termina el rastreo.
+    # Retorna 'None' si todo funciona bien y si retorna 'False' significa que hubo un error interno (por nuestra clase Tracer o subclases).
+    def __exit__(self, exc_tp, exc_value, exc_traceback):
+        sys.settrace(self.original_trace_function)
+
+       # Note que debemos retornar un valor 'False' para indicar que hubo un error interno y levantar las excepciones correspondientes.
+        if self.is_internal_error(exc_tp, exc_value, exc_traceback):
+            return False
+        else:
+            # Significa que todo funciona bien
+            return None
--- Tarea3/tracer/tracking.py
+++ Tarea3/tracer/tracking.py
@@ -9,24 +9,25 @@ Use de la siguiente forma: with Tracer(): block() """
 
 
 class Tracer(StackInspector):
-
     # Inicializa las variables para rastrear un bloque de codigo y mandar logs a 'file'
     def __init__(self, *, file=sys.stdout):
-        self.original_trace_function = None
+        super().__init__(None, self.traceit)
         self.file = file
 
-    """Funcion de rastreo que llama a imprimir los eventos y evita que rastreemos nuestros propios metodos
+    """Funcion de rastreo que llama a imprimir los eventos y evita que rastreemos
+    nuestros propios metodos
     Por el momento esta funcion envuelve a:
     - Un evento (event): Es una cadena que puede tener los siguientes valores:
-        * 'line' 	- una nueva linea siendo ejecutada
-        * 'call' 	- una funcion que acaba de ser llamada
-        * 'return' 	- el retorno de una funcion
-    - Un frame: El frame de ejecucion actual, es decir, un objeto de rastreo (e.g., una funcion). 
+    * 'line' - una nueva linea siendo ejecutada
+    * 'call' - una funcion que acaba de ser llamada
+    * 'return' - el retorno de una funcion
+    - Un frame: El frame de ejecucion actual, es decir, un objeto de rastreo (e.g.,
+    una funcion).
     Un frame tiene los siguientes atributos:
-        * frame.f_lineno - la linea actual
-        * frame.f_locals - las variables actuales (es un dictionary)
-        * frame.f_code 	- el codigo actual (Code) que tiene el atributo frame.f_code.co_name (el nombre del la funcion actual)
-
+    * frame.f_lineno - la linea actual
+    * frame.f_locals - las variables actuales (es un dictionary)
+    * frame.f_code - el codigo actual (Code) que tiene el atributo
+    frame.f_code.co_name (el nombre del la funcion actual)
     """
 
     def traceit(self, frame, event, arg):
@@ -34,40 +35,19 @@ class Tracer(StackInspector):
             # No rastrearemos nuestros propios metodos
             pass
         else:
-            self.log(event, frame.f_lineno, frame.f_code.co_name, frame.f_locals)
-        return self.traceit
+            self.log(event, frame.f_lineno,
+                     frame.f_code.co_name, frame.f_locals)
+            return self.traceit
 
     # Es como un print(), pero siempre lo manda a archivo dado en la inicializacion (self.file), que por defecto es la consola.
     def log(self, *objects, sep: str = ' ', end: str = '\n', flush=True):
         print(*objects, sep=sep, end=end, file=self.file, flush=flush)
-
-    # Esta funcion se llama al comienzo de un bloque 'with' y comienza con el rastreo
-    def __enter__(self):
-        self.original_trace_function = sys.gettrace()
-        sys.settrace(self.traceit)
-
-        # La linea de abajo que esta comentada tambien permite rastrear al bloque actual
-        # inspect.currentframe().f_back.f_trace = self.traceit
-        return self
-
-    # Esta funcion se llama al final de un bloque 'with' y termina el rastreo.
-    # Retorna 'None' si _todo funciona bien y si retorna 'False' significa que hubo un error interno (por nuestra clase Tracer o subclases).
-    def __exit__(self, exc, exc_value, exc_traceback):
-        sys.settrace(self.original_trace_function)
-
-        # Note que debemos retornar un valor 'False' para indicar que hubo un error interno y levantar las excepciones correspondientes.
-        if self.is_internal_error(exc, exc_value, exc_traceback):
-            return False
-        else:
-            # Significa que _todo funciona bien
-            return None
 
 
 def remove_html_tags(s):
     tag = False
     quote = False
     out = ""
-
     for c in s:
         if c == '<' and not quote:
             tag = True
@@ -77,7 +57,6 @@ def remove_html_tags(s):
             quote = not quote
         elif not tag:
             out = out + c
-
     return out
 
 
