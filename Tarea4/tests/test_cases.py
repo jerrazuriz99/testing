3e87e2681026c0bb284dcfcfcad9dd3eddfd90f2
JuanSa <juan.vg@uc.cl>
2023-04-18 17:59:59 -0400

    Subiendo actividad 2 entera

--- Tarea4/tests/test_cases.py
+++ Tarea4/tests/test_cases.py
@@ -53,41 +53,19 @@ class Tests(TestCase):
         result.append(clock.str())
         invariant_result.append(clock.invariant())
         clock2 = clock.clone()
-        for i in range(100):
+        for i in range(1440):
             clock.increment()
+            """ print(f"incrementando clock en iteración {i}") """
             clock2.increment()
 
             result.append(clock.str())
             invariant_result.append(clock.invariant())
 
 
-        excepted_result = ['00:00', '00:01', '00:02', '00:03',
-                           '00:04', '00:05', '00:06', '00:07',
-                           '00:08', '00:09', '00:10', '00:11',
-                           '00:12', '00:13', '00:14', '00:15',
-                           '00:16', '00:17', '00:18', '00:19',
-                           '00:20', '00:21', '00:22', '00:23',
-                           '00:24', '00:25', '00:26', '00:27',
-                           '00:28', '00:29', '00:30', '00:31',
-                           '00:32', '00:33', '00:34', '00:35',
-                           '00:36', '00:37', '00:38', '00:39',
-                           '00:40', '00:41', '00:42', '00:43',
-                           '00:44', '00:45', '00:46', '00:47',
-                           '00:48', '00:49', '00:50', '00:51',
-                           '00:52', '00:53', '00:54', '00:55',
-                           '00:56', '00:57', '00:58', '00:59',
-                           '01:00', '01:01', '01:02', '01:03',
-                           '01:04', '01:05', '01:06', '01:07',
-                           '01:08', '01:09', '01:10', '01:11',
-                           '01:12', '01:13', '01:14', '01:15',
-                           '01:16', '01:17', '01:18', '01:19',
-                           '01:20', '01:21', '01:22', '01:23',
-                           '01:24', '01:25', '01:26', '01:27',
-                           '01:28', '01:29', '01:30', '01:31',
-                           '01:32', '01:33', '01:34', '01:35',
-                           '01:36', '01:37', '01:38', '01:39', '01:40']
+        excepted_result = list(map(lambda x: f"{x//60:02d}:{x%60:02d}", range(1440)))
+        excepted_result.append("00:00")
 
-        excepted_invariant = [True for i in range(101)]
+        excepted_invariant = [True for i in range(1441)]
 
         self.assertEqual(result, excepted_result)
         self.assertEqual(invariant_result, excepted_invariant)
@@ -101,40 +79,17 @@ class Tests(TestCase):
         invariant_result.append(clock.invariant())
         clock2 = factory.create("hh:mm:ss")
         clock3 = factory.create("hh:mm:ss:mmmm")
-        for i in range(100):
+        for i in range(1440):
             clock.increment()
             clock2.increment()
             clock3.increment()
             result.append(clock.str())
             invariant_result.append(clock.invariant())
         
-        excepted_result = ['00:00', '00:01', '00:02', '00:03',
-                           '00:04', '00:05', '00:06', '00:07',
-                           '00:08', '00:09', '00:10', '00:11',
-                           '00:12', '00:13', '00:14', '00:15',
-                           '00:16', '00:17', '00:18', '00:19',
-                           '00:20', '00:21', '00:22', '00:23',
-                           '00:24', '00:25', '00:26', '00:27',
-                           '00:28', '00:29', '00:30', '00:31',
-                           '00:32', '00:33', '00:34', '00:35',
-                           '00:36', '00:37', '00:38', '00:39',
-                           '00:40', '00:41', '00:42', '00:43',
-                           '00:44', '00:45', '00:46', '00:47',
-                           '00:48', '00:49', '00:50', '00:51',
-                           '00:52', '00:53', '00:54', '00:55',
-                           '00:56', '00:57', '00:58', '00:59',
-                           '01:00', '01:01', '01:02', '01:03',
-                           '01:04', '01:05', '01:06', '01:07',
-                           '01:08', '01:09', '01:10', '01:11',
-                           '01:12', '01:13', '01:14', '01:15',
-                           '01:16', '01:17', '01:18', '01:19',
-                           '01:20', '01:21', '01:22', '01:23',
-                           '01:24', '01:25', '01:26', '01:27',
-                           '01:28', '01:29', '01:30', '01:31',
-                           '01:32', '01:33', '01:34', '01:35',
-                           '01:36', '01:37', '01:38', '01:39', '01:40']
+        excepted_result = list(map(lambda x: f"{x//60:02d}:{x%60:02d}", range(1440)))
+        excepted_result.append("00:00")
 
-        excepted_invariant = [True for i in range(101)]
+        excepted_invariant = [True for i in range(1441)]
 
         self.assertEqual(result, excepted_result)
         self.assertEqual(invariant_result, excepted_invariant)
